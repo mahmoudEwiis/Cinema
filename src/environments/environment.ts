7df201bf61787f7445ad425cb710a5bfba995030
Mahmoud Ewis <60224561+mahmoudEwiis@users.noreply.github.com>
2022-03-11 00:27:41 +0200

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,15 +4,7 @@
 
 export const environment = {
   production: false,
-  firebaseConfig: {
-    apiKey: "AIzaSyCNRPAX14lxo1PsmS9G2l_RZvJPdUHPBI4",
-    authDomain: "movie-3f754.firebaseapp.com",
-    projectId: "movie-3f754",
-    storageBucket: "movie-3f754.appspot.com",
-    messagingSenderId: "263850414528",
-    appId: "1:263850414528:web:976e13eca8b226ca24b825",
-    measurementId: "G-BSJK92V35Q"
-  }
+
 };
 
 /*
